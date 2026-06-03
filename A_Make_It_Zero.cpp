f09171c8f665a6727cecc222400c322e1d3d2519
Nitya Upadhyay <nityaupadhyay1221@gmail.com>
2026-06-04 01:34:55 +0530

    cf

--- A_Make_It_Zero.cpp
+++ A_Make_It_Zero.cpp
@@ -2,11 +2,25 @@
 #include <vector>
 
 using namespace std;
-int solve(){
+void solve(){
     int n;
     cin>>n;
     vector<int>a(n);
     for(int i=0;i<n;i++)cin>>a[i];
+    if(n&1){
+        cout<<4<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        //cout<<1<<" "<<n-1<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        cout<<n-1<<" "<<n<<endl;
+        cout<<n-1<<" "<<n<<endl;
+
+    }
+    else{
+        cout<<2<<endl;
+        cout<<1<<" "<<n<<endl;
+        cout<<1<<" "<<n<<endl;
+    }
 
         
 }
@@ -16,6 +30,7 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-       cout <<solve() << endl;
+      solve();
+      //cout<<endl;
     }
 }
